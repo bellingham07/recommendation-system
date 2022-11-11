7307be221e0c428e10956c05d7f45eddb86f37ac
cao <75364055@qq.com>
2022-11-11 19:46:55 +0800

    ojbk

--- sys-eshop-front/src/main/java/com/example/eshopFront/EShopFrontApplication.java
+++ sys-eshop-front/src/main/java/com/example/eshopFront/EShopFrontApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"com.example.common"})
 @MapperScan("com.example.common.dao")
 public class EShopFrontApplication {
+    //dfafa
     public static void main(String[] args) {
         SpringApplication.run(EShopFrontApplication.class, args);
     }
